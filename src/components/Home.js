8b197eaf0666b874985527f87956519c7ea69906
Shaun Omari <shaunomari72@gmail.com>
2023-10-25 13:27:30 +0300

    compiled successfully

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,19 @@ import { name, city } from "../data/data.js";
 
 function Home() {
   // update the JSX being returned!
-  return <div>Home</div>;
+  const divStyle = {
+    color: 'firebrick'
+  }
+  return (
+    <div id="home">
+      <h1 style={divStyle}>
+        {name} is a web developer from {city}
+      </h1>
+
+    </div>
+
+  );
+  
 }
 
 export default Home;
